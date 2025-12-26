fcd01b6de905a3d2040d499954dd4afb965f7f65
mahmudtech <mahmud.irfan.dev@gmail.com>
2025-12-26 20:31:43 +0600

    update

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { getAllData } from "./data";
 export default function App() {
   const [users, setUsers] = useState(getAllData());
   const [name, setName] = useState("");
+  const [searchText, setSearchText] = useState("");
   const handleAddName = () => {
     const newName = {
       id: crypto.randomUUID(),
@@ -18,6 +19,10 @@ export default function App() {
     const affterDelete = users.filter((user) => user.id !== deleteId);
     setUsers(affterDelete);
   };
+
+  const filterUser = users.filter((user) =>
+    user.name.toLowerCase().includes(searchText.toLowerCase())
+  );
   return (
     <div>
       <div>
@@ -28,21 +33,38 @@ export default function App() {
         />
         <button onClick={handleAddName}>Add</button>
       </div>
+      <br />
+      <div>
+        <input
+          type="search"
+          placeholder="Search"
+          value={searchText}
+          onChange={(e) => setSearchText(e.target.value)}
+        />
+      </div>
       {users.length === 0 ? (
         <>
           <p>No data found</p>
         </>
+      ) : filterUser.length === 0 ? (
+        <>No data found</>
       ) : (
         <>
           <ul>
-            {users.map((user) => (
-              <li key={user.id}>
-                {user.name}{" "}
-                <button onClick={() => handleDeleteItem(user.id)}>
-                  Deleted
-                </button>
-              </li>
-            ))}
+            {filterUser
+              .filter((user) =>
+                user.name.toLowerCase().includes(searchText.toLowerCase())
+              )
+              .map((user) => {
+                return (
+                  <li key={user.id}>
+                    {user.name}{" "}
+                    <button onClick={() => handleDeleteItem(user.id)}>
+                      delete
+                    </button>
+                  </li>
+                );
+              })}
           </ul>
         </>
       )}
