06788cf2d8e12a1752a4d1aade043096b6b42237
mahmudtech <mahmud.irfan.dev@gmail.com>
2025-12-26 18:02:53 +0600

    mode init

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,79 @@
 import { useState } from "react";
 
-export default function App() {
-  const [name, setName] = useState("Milon Hobe Kotodine");
-  const [isEditing, setIsEditing] = useState(false);
+let nextId = 0;
 
-  const handleTextUpdate = (e) => {
-    setName(e.target.value);
+export default function AddItem() {
+  const [name, setName] = useState("");
+  const [artists, setArtists] = useState([]);
+  const [editingId, setEditingId] = useState(null);
+  const [editingName, setEditingName] = useState("");
+
+  const addArtists = () => {
+    setArtists([...artists, { id: nextId++, name }]);
+    setName("");
   };
 
+  const handleDeleteItem = (deleteId) => {
+    setArtists(artists.filter((a) => a.id !== deleteId));
+  };
+
+  const handleReverseClick = () => {
+    setArtists([...artists].reverse());
+  };
+
+  // 👉 Edit শুরু
+  const handleEditItem = (artist) => {
+    setEditingId(artist.id);
+    setEditingName(artist.name);
+  };
+
+  // 👉 Save
   const handleSave = () => {
-    setIsEditing(false);
+    setArtists(
+      artists.map((a) => (a.id === editingId ? { ...a, name: editingName } : a))
+    );
+    setEditingId(null);
+    setEditingName("");
   };
-  return isEditing ? (
+
+  return (
     <>
-      <input type="text" value={name} onChange={handleTextUpdate} />
-      <button onClick={handleSave}>Save</button>
-    </>
-  ) : (
-    <>
-      {" "}
-      <p>
-        {name}
-        <button onClick={() => setIsEditing(true)}>Edit</button>
-      </p>
+      <h1>Inspiring sculptors:</h1>
+
+      <button onClick={handleReverseClick}>Reverse</button>
+
+      <br />
+      <br />
+
+      <input value={name} onChange={(e) => setName(e.target.value)} />
+      <button onClick={addArtists}>Add</button>
+
+      <br />
+      <br />
+
+      {artists.map((artist) => (
+        <ul key={artist.id}>
+          <li>
+            {editingId === artist.id ? (
+              <>
+                <input
+                  value={editingName}
+                  onChange={(e) => setEditingName(e.target.value)}
+                />
+                <button onClick={handleSave}>Save</button>
+              </>
+            ) : (
+              <>
+                {artist.name}
+                <button onClick={() => handleDeleteItem(artist.id)}>
+                  Delete
+                </button>
+                <button onClick={() => handleEditItem(artist)}>Edit</button>
+              </>
+            )}
+          </li>
+        </ul>
+      ))}
     </>
   );
 }
