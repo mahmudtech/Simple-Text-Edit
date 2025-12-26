1744ac06441b7768549feb38bf4568b1a24d905f
mahmudtech <mahmud.irfan.dev@gmail.com>
2025-12-26 19:20:37 +0600

    user list aded

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,29 @@
+import { useState } from "react";
 import { getAllData } from "./data";
 
 export default function App() {
-  const users = getAllData();
+  const [users, setUsers] = useState(getAllData());
+  const [name, setName] = useState("");
+  const handleAddName = () => {
+    const newName = {
+      id: crypto.randomUUID(),
+      name: name,
+    };
+
+    setUsers([...users, newName]);
+
+    setName("");
+  };
   return (
     <div>
+      <div>
+        <input
+          type="text"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <button onClick={handleAddName}>Add</button>
+      </div>
       <ul>
         {users.map((user) => (
           <li key={user.id}>{user.name}</li>
